ae6bc6a0853c9c317dfcc54a2f629c8b2160f33a
Maksim Gusev <51376126+AlanTheKnight@users.noreply.github.com>
2024-03-16 13:03:17 +0300

    DB Lab #1 Update

--- Databases/Lab 1/delete_tables.sql
+++ Databases/Lab 1/delete_tables.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS "info_spread";
 DROP TABLE IF EXISTS "cities_information";
 DROP TABLE IF EXISTS "friendships";
 DROP TABLE IF EXISTS "information";
-DROP TABLE IF EXISTS "persons";
+DROP TABLE IF EXISTS "persons" CASCADE;
 DROP TABLE IF EXISTS "cities";
